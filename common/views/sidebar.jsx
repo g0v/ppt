09fda72a1b4028a3c4244cc0be671a7cd24beb91
MrOrz <johnsonliang7@gmail.com>
2015-02-08 10:59:55 +0800

    Sidebar menu content & styling

--- common/views/sidebar.jsx
+++ common/views/sidebar.jsx
@@ -5,35 +5,92 @@ var Sidebar = React.createClass({
     $(".ui.sidebar").sidebar("toggle");
   },
   render: function(){
+
+    var sidebarTextColor = 'rgba(255, 255, 255, .82)';
+
+    var listStyle = {
+      // background: 'transparent',
+    },
+    listItemStyle = {
+      padding: 8
+    },
+    segmentStyle = {
+      // padding: 8,
+    },
+    segmentContainerStyle = {
+      boxShadow: 'none',
+      background: 'transparent',
+      color: sidebarTextColor
+    },
+    headerStyle = {
+      color: sidebarTextColor
+    },
+    descriptionStyle = {
+      fontSize: 12,
+      color: sidebarTextColor
+    }
+
     return (
       <div className="ui large green inverted vertical sidebar menu">
-        <a className="active item">
-          Dogs Weekly <span className="ui label">213</span>
-        </a>
-        <a className="item">
-          Joystiq <span className="ui label">113</span>
-        </a>
-        <div className="item">
-          <b>Archived Feeds</b>
-          <div className="menu">
-            <a className="item">
-              Engadget <span className="ui label">11</span>
-            </a>
-            <a className="item">
-              NY Times Tech Blog <span className="ui label">21</span>
-            </a>
-          </div>
-        </div>
-        <a className="item">
-          <i className="bookmark icon"></i> Favorites
-        </a>
-        <div className="ui dropdown item">
-          <i className="add icon"></i> New
-          <div className="menu">
-            <a className="item"><i className="rss icon"></i> Feed</a>
-            <a className="item"><i className="tag icon"></i> Tag</a>
-            <a className="item"><i className="folder icon"></i> Group</a>
-          </div>
+        <div className="ui segment" style={segmentContainerStyle}>
+          <section className="ui vertical segment" style={segmentStyle}>
+            <div className="ui list" style={listStyle}>
+              <a className="item" style={listItemStyle}>
+                <img className="ui avatar image" src="http://semantic-ui.com/images/avatar/small/daniel.jpg"/>
+                <div className="content">
+                  <div className="header" style={headerStyle}>胡自強</div>
+                  <div className="description" style={descriptionStyle}>台中市長・2010-2014・43 承諾</div>
+                </div>
+              </a>
+
+              <a className="item" style={listItemStyle}>
+                <img className="ui avatar image" src="http://semantic-ui.com/images/avatar/small/daniel.jpg"/>
+                <div className="content">
+                  <div className="header" style={headerStyle}>胡自強</div>
+                  <div className="description" style={descriptionStyle}>台中市長・2010-2014・43 承諾</div>
+                </div>
+              </a>
+            </div>
+          </section>
+
+          <section className="ui vertical segment" style={segmentStyle}>
+            <h5 style={listItemStyle}>縣市長・2014 - 2018</h5>
+            <div className="ui list" style={listStyle}>
+              <a className="item" style={listItemStyle}>
+                <img className="ui avatar image" src="http://semantic-ui.com/images/avatar/small/daniel.jpg"/>
+                <div className="content">
+                  <div className="header" style={headerStyle}>胡自強</div>
+                  <div className="description" style={descriptionStyle}>台中市長・2010-2014・43 承諾</div>
+                </div>
+              </a>
+
+              <a className="item" style={listItemStyle}>
+                <img className="ui avatar image" src="http://semantic-ui.com/images/avatar/small/daniel.jpg"/>
+                <div className="content">
+                  <div className="header" style={headerStyle}>胡自強</div>
+                  <div className="description" style={descriptionStyle}>台中市長・2010-2014・43 承諾</div>
+                </div>
+              </a>
+            </div>
+          </section>
+
+          <section className="ui vertical segment" style={segmentStyle}>
+            <div className="ui list" style={listStyle}>
+              <a className="item" style={listItemStyle}>
+                <img className="ui avatar image" src="http://semantic-ui.com/images/avatar/small/daniel.jpg"/>
+                <div className="content">
+                  <div className="header" style={headerStyle}>新增施政單位⋯⋯</div>
+                </div>
+              </a>
+
+              <a className="item" style={listItemStyle}>
+                <img className="ui top avatar image" src="http://semantic-ui.com/images/avatar/small/daniel.jpg"/>
+                <div className="content">
+                  <div className="header" style={headerStyle}>關於政治承諾追蹤網</div>
+                </div>
+              </a>
+            </div>
+          </section>
         </div>
       </div>
     );
