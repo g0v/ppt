d00bf2893761ac0007beaa1c5c380421932565c0
MrOrz <johnsonliang7@gmail.com>
2015-05-31 21:48:23 +0800

    Make server runnable

---
+++ client/config/webpack.js
@@ -0,0 +1,85 @@
+var webpack = require('webpack'),
+    ExtractText = require('extract-text-webpack-plugin'),
+    packageJson = require('../../package.json');
+
+var isProduction = process.env.NODE_ENV === 'production';
+
+// Base config
+//
+var webpackCfg = {
+  entry: {
+    'client': './client/js/client.jsx',
+  },
+  output: {
+    // __dirname is the path of webpack.js
+    path: __dirname + '/../build',
+    filename: ( isProduction ? '[hash].js' : 'client.js')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.styl$/,
+        loader: ExtractText.extract("css?sourceMap!stylus")
+      },
+      {
+        test: /\.(?:jpg|png|gif|eot|svg|ttf|woff|otf)$/,
+        loader: "file-loader"
+      },
+      {
+        test: /\.jsx$/, loader: "babel-loader"
+      },
+      {
+        test: /common\/.+\.js$/, loader: 'babel-loader'
+      },
+      {
+        test: /client\/js\/.+\.js$/, loader: 'babel-loader', exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new ExtractText( isProduction ? "[hash].css" : "client.css" )
+  ],
+  debug: !isProduction,
+  externals: {
+    // require("jquery") is external and available
+    //  on the global var jQuery
+    "jquery": "jQuery"
+  }
+};
+
+// Other env-based configs
+//
+if( isProduction ){
+  webpackCfg.plugins.push(new webpack.optimize.UglifyJsPlugin({
+    sourceMap: false
+  }));
+}else{
+  webpackCfg.devtool = '#source-map';
+
+  // Hot module replacement setup
+  // Ref:
+  // http://webpack.github.io/docs/webpack-dev-server.html#combining-with-an-existing-server
+  // http://gaearon.github.io/react-hot-loader/#enabling-hot-module-replacement
+  //
+  webpackCfg.entry.client = [
+    // Instruct socket.io in weboack-dev-server to connect to hostname-agnostic '/'
+    'webpack-dev-server/client?/',
+    'webpack/hot/dev-server',
+    webpackCfg.entry.client
+  ];
+
+  webpackCfg.devServer = {
+    host: '0.0.0.0',
+    port: packageJson.config.webpackDevServerPort,
+    proxy: {
+      "*": "http://127.0.0.1:" + packageJson.config.apiServerPort
+    },
+    hot: true,
+    watchDelay: 2000 // Wait until nodemon restarts
+  };
+
+  webpackCfg.plugins.push(new webpack.HotModuleReplacementPlugin());
+  webpackCfg.output.publicPath = '/build/'
+}
+
+module.exports = webpackCfg;
--- server/utils/catchUnhandledPromiseRejections.js
+++ server/utils/catchUnhandledPromiseRejections.js
@@ -1,12 +1,14 @@
 // Catch all unhandled promise rejections and print error.
 // Ref: https://iojs.org/api/process.html#process_event_unhandledrejection
 //
+var debug = require('debug')('ppt:unhandledRejection');
+
 process.on('unhandledRejection', function(reason, promise) {
   if (reason.stack) {
     // Error object, has stack info
-    console.error('[Unhandled Rejection]', reason.stack);
+    debug(reason.stack);
   } else {
-    console.error('[Unhandled Rejection] Reason:', reason);
+    debug('Reason:', reason);
   }
-  console.error('[Unhandled Rejection] Promise:', promise);
+  debug('Promise:', promise);
 });
