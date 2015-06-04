c2ad324014823bd5bfeb81487361c289dc071e48
MrOrz <johnsonliang7@gmail.com>
2015-06-04 09:40:20 +0800

    Remove unused resolver.

--- common/fluxibleApp.js
+++ common/fluxibleApp.js
@@ -1,33 +1,11 @@
 // Fluxible app instance
 
 import Fluxible from 'fluxible';
-import App from './views/App.jsx';
-import MetaStore from './stores/MetaStore';
-import RouteStore from './stores/RouteStore';
-import Resolver from './utils/resolver';
 
-class Flux extends Fluxible {
-  constructor() {
-    super({
-      component: App,
-      stores: [
-          RouteStore,
-          MetaStore
-      ]
-    });
-  }
-
-  // Arguments the context instance with Resolver's methods.
-  // Ref: https://github.com/iam4x/isomorphic-flux-boilerplate/blob/master/app/utils/flux.js
-  //
-  createContext() {
-    var context = super.createContext(),
-        resolver = new Resolver;
-    context.resolve = promise => resolver.resolve(promise);
-    context.renderToString = (...args) => resolver.renderToString(...args);
-
-    return context;
-  }
-}
-
-module.exports = new Flux();
+module.exports = new Fluxible({
+  component: require('./views/App.jsx'),
+  stores: [
+    require('./stores/RouteStore'),
+    require('./stores/MetaStore')
+  ]
+});
