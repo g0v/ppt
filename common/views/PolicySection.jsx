e313cbceedf841e141bcfca778fd78ff7ad326ee
transedward <transedward@gmail.com>
2015-06-05 21:06:39 +0800

    sorry, forget to clear unused function

--- common/views/PolicySection.jsx
+++ common/views/PolicySection.jsx
@@ -7,34 +7,6 @@ import debug from 'debug';
 const {Transitions} = mui.Styles;
 const debugPolicySection = debug('ppt:PolicySection');
 
-// fade out
-function fadeOut(el){
-  el.style.opacity = 1;
-
-  (function fade() {
-    if ((el.style.opacity -= .1) < 0) {
-      el.style.display = "none";
-    } else {
-      requestAnimationFrame(fade);
-    }
-  })();
-}
-
-// fade in
-
-function fadeIn(el, display){
-  el.style.opacity = 0;
-  el.style.display = display || "block";
-
-  (function fade() {
-    var val = parseFloat(el.style.opacity);
-    if (!((val += .1) > 1)) {
-      el.style.opacity = val;
-      requestAnimationFrame(fade);
-    }
-  })();
-}
-
 class PolicySection extends React.Component {
 
   constructor(){
