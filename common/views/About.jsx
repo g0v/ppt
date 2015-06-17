3fad367a17c2bde6e5da1f65993891e342e3334a
MrOrz <johnsonliang7@gmail.com>
2015-06-17 14:30:01 +0800

    Remove styles.js. Implement getStyles() for each view components.

--- common/views/About.jsx
+++ common/views/About.jsx
@@ -1,11 +1,23 @@
 var React = require('react'),
-    Transmit = require('react-transmit'),
-    styles = require('./styles.js');
+    mui = require('material-ui'),
+    Transmit = require('react-transmit');
+
+const Spacing = mui.Styles.Spacing;
 
 var About = React.createClass({
+  getStyles() {
+    return {
+      root: {
+        paddingTop: Spacing.desktopKeylineIncrement
+      }
+    };
+  },
+
   render: function(){
+    const styles = this.getStyles();
+
     return (
-      <div className="full height main container" style={styles.mainContainer}>
+      <div className="full height main container" style={styles.root}>
         <h1>關於政治承諾追蹤網</h1>
         <p>Hello world!</p>
       </div>
