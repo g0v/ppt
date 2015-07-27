19215a969285df6d15f1be873147644b4661844d
transedward <transedward@gmail.com>
2015-07-27 19:21:23 +0800

    divide commitment into two parts to make code cleaner

--- common/views/Commitment.jsx
+++ common/views/Commitment.jsx
@@ -1,103 +1,16 @@
 var React = require('react'),
-    debug = require('debug')('ppt:promise'),
+    debug = require('debug')('ppt:commiment'),
     Loading = require('./Loading.jsx'),
-    Fluxible = require('fluxible'),
     Transmit = require('react-transmit');
 
 import {handleRoute} from 'fluxible-router';
-import { findAll, progressIconPicker } from '../utils/';
-import mui, { List, ListItem, ListDivider, Avatar } from 'material-ui';
+import { findAll } from '../utils/';
+import { ListDivider } from 'material-ui';
 import ForwardIcon from 'material-ui/lib/svg-icons/navigation/arrow-forward';
 import AutoLinkText from 'react-autolink-text';
 import pptColors from '../styles/color';
 import pptSpacing from '../styles/spacing';
-
-var ProgressReport = React.createClass({
-  propTypes: {
-    history: React.PropTypes.array.isRequired,
-    ratings: React.PropTypes.array.isRequired,
-    isExpanded: React.PropTypes.bool
-  },
-  getDefaultProps: function() {
-    return {isExpanded: false};
-  },
-  getStyles() {
-    return {
-      latestFromHistoryBrief: {
-        color: pptColors.darkGray,
-        fontSize: '16px'
-      }
-    }
-  },
-  render: function() {
-    let styles = this.getStyles();
-    var voteCount = {notyet: 0, doing: 0, done: 0},
-        maxVote = 0,
-        ratingElements = [],
-        mostVoteProgress;
-    // need to check if needed expanded, otherwise don't compute
-    this.props.isExpanded && this.props.ratings.forEach(function(rating, idx) {
-      var contentText, progressText, commentText;
-
-      voteCount[rating.progress] += 1;
-      if (maxVote <= voteCount[rating.progress]) {
-        mostVoteProgress = rating.progress;
-        maxVote = voteCount[rating.progress];
-      }
-
-      switch (rating.progress){
-        case 'doing':
-          progressText = (<span style={{color: pptColors.primaryYellow}}>正在做</span>);
-          break;
-        case 'done':
-          progressText = (<span style={{color: pptColors.primaryBlue}}>已完成</span>);
-          break;
-        case 'notyet':
-          progressText = (<span style={{color: pptColors.primaryRed}}>還沒做</span>);
-          break;
-      }
-
-      if (rating.comment) {
-        contentText = '認為';
-        commentText = '：' + rating.comment;
-      }else {
-        contentText = '認為承諾';
-        commentText = '。'
-      }
-
-      let userAvatar = (
-        <Avatar src={rating.User.avatar} />
-      );
-      ratingElements.unshift(
-        <ListItem leftAvatar={userAvatar} key={idx} secondaryText=
-          {<div>
-            <p> {rating.User.name} </p>
-            &nbsp;{contentText}{progressText}{commentText}
-          </div>} >
-        </ListItem>
-      );
-    });
-
-    var latestFromHistory = this.props.history[this.props.history.length - 1];
-
-    return (
-      <List>
-        <ListItem leftIcon={progressIconPicker(mostVoteProgress)}
-                secondaryText={
-                  <div>
-                    <span style={styles.latestFromHistoryBrief}>
-                      {latestFromHistory.brief}
-                    </span>
-                    <p style={{color: pptColors.primaryBlue}}>佐證連結</p>
-                  </div>}
-                secondaryTextLines={2}>
-        </ListItem>
-        {ratingElements}
-        <ListDivider inset={true} />
-      </List>
-    )
-  }
-})
+import ProgressReport from './ProgressReport.jsx'
 
 var Commitment = React.createClass({
 
@@ -162,9 +75,8 @@ var Commitment = React.createClass({
                         ratings={report.ProgressRatings}
                         isExpanded={false}/>
       );
-    });
-
-    var progressReportElems = [];
+    }),
+        progressReportElems = [];
 
     if (latestProgressReport) {
       progressReportElems.push(
---
+++ common/views/ProgressReport.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import {handleRoute} from 'fluxible-router';
+import { progressIconPicker } from '../utils/';
+import { List, ListItem, ListDivider, Avatar } from 'material-ui';
+import pptColors from '../styles/color';
+
+var ProgressReport = React.createClass({
+  propTypes: {
+    history: React.PropTypes.array.isRequired,
+    ratings: React.PropTypes.array.isRequired,
+    isExpanded: React.PropTypes.bool
+  },
+  getDefaultProps: function() {
+    return {isExpanded: false};
+  },
+  getStyles() {
+    return {
+      latestFromHistoryBrief: {
+        color: pptColors.darkGray,
+        fontSize: '16px'
+      }
+    }
+  },
+  render: function() {
+    let styles = this.getStyles();
+    var voteCount = {notyet: 0, doing: 0, done: 0},
+        maxVote = 0,
+        ratingElements = [],
+        mostVoteProgress;
+    // need to check if needed expanded, otherwise don't compute
+    this.props.isExpanded && this.props.ratings.forEach(function(rating, idx) {
+      var contentText, progressText, commentText;
+
+      voteCount[rating.progress] += 1;
+      if (maxVote <= voteCount[rating.progress]) {
+        mostVoteProgress = rating.progress;
+        maxVote = voteCount[rating.progress];
+      }
+
+      switch (rating.progress){
+        case 'doing':
+          progressText = (<span style={{color: pptColors.primaryYellow}}>正在做</span>);
+          break;
+        case 'done':
+          progressText = (<span style={{color: pptColors.primaryBlue}}>已完成</span>);
+          break;
+        case 'notyet':
+          progressText = (<span style={{color: pptColors.primaryRed}}>還沒做</span>);
+          break;
+      }
+
+      if (rating.comment) {
+        contentText = '認為';
+        commentText = '：' + rating.comment;
+      }else {
+        contentText = '認為承諾';
+        commentText = '。'
+      }
+
+      let userAvatar = (
+        <Avatar src={rating.User.avatar} />
+      );
+      ratingElements.unshift(
+        <ListItem leftAvatar={userAvatar} key={idx} secondaryText=
+          {<div>
+            <p> {rating.User.name} </p>
+            &nbsp;{contentText}{progressText}{commentText}
+          </div>} >
+        </ListItem>
+      );
+    });
+
+    var latestFromHistory = this.props.history[this.props.history.length - 1];
+
+    return (
+      <List>
+        <ListItem leftIcon={progressIconPicker(mostVoteProgress)}
+                secondaryText={
+                  <div>
+                    <span style={styles.latestFromHistoryBrief}>
+                      {latestFromHistory.brief}
+                    </span>
+                    <p style={{color: pptColors.primaryBlue}}>佐證連結</p>
+                  </div>}
+                secondaryTextLines={2}>
+        </ListItem>
+        {ratingElements}
+        <ListDivider inset={true} />
+      </List>
+    )
+  }
+})
+
+module.exports = handleRoute(ProgressReport);
