bd5a2b1e792bbf432a73e67a379e7ba353665fc8
transedward <transedward@gmail.com>
2015-08-01 21:28:49 +0800

    add check if wasVisited for welcomeSection and let effect works

--- common/views/Home.jsx
+++ common/views/Home.jsx
@@ -2,59 +2,41 @@ import React from 'react';
 import Transmit from 'react-transmit';
 import { Avatar, Card, CardTitle, CardText ,CardActions, ListItem, ListDivider,
   RaisedButton } from 'material-ui';
-import DoneIcon from 'material-ui/lib/svg-icons/action/done';
 import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
 import ForwardIcon from 'material-ui/lib/svg-icons/navigation/arrow-forward';
+import WelcomeSection from './WelcomeSection.jsx';
 import pptColors from '../styles/color';
 import pptSpacing from '../styles/spacing';
 
-var Home = React.createClass({
+class Home extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      wasVisited: true
+    };
+  }
+
+  componentDidMount() {
+    if (!(window.localStorage.getItem('wasVisited'))) {
+      window.localStorage.setItem('wasVisited', false)
+      this.setState({
+        wasVisited: false
+      });
+    }
+  }
+
+  onKnownTouchTap() {
+    window.localStorage.setItem('wasVisited', true)
+    React.findDOMNode(this.refs.welcomeSection).style.height = 0;
+  }
+
   getStyles() {
     return {
       root: {
         width: '100%',
         height: '100%',
         paddingTop: pptSpacing.appBarHeight
-      },
-      welcomeSection: {
-        root: {
-          backgroundColor: pptColors.primaryBlue,
-          height: 224,
-          width: '100%',
-          maxWidth: 960,
-          margin: '8px auto'
-        },
-        h1: {
-          marginTop: 22,
-          marginLeft: 17,
-          fontSize: 24,
-          color: pptColors.darkWhite
-        },
-        h3: {
-          fontSize: 15,
-          color: pptColors.darkWhite
-        },
-        h4: {
-          marginTop: 7,
-          marginLeft: 17,
-          fontSize: 15,
-          color: pptColors.darkWhite
-        },
-        h5: {
-          marginLeft: 17,
-          fontSize: 12,
-          color: pptColors.lightWhite
-        },
-        divider: {
-          marginTop: 11,
-          height: 2,
-          backgroundColor: pptColors.faintWhite
-        },
-        doneIcon: {
-          width: 24,
-          height: 24,
-          fill: pptColors.white
-        }
       },
       recentUpdate: {
         root: {
@@ -102,23 +84,15 @@ var Home = React.createClass({
         }
       }
     };
-  },
+  }
 
-  render: function(){
+  render() {
     const styles = this.getStyles();
 
     return (
       <div style={styles.root}>
-        <Card style={styles.welcomeSection.root}>
-          <h1 style={styles.welcomeSection.h1}>一起來監督施政吧</h1>
-          <h4 style={styles.welcomeSection.h4}>查詢政府首長施政進度</h4>
-          <h5 style={styles.welcomeSection.h5}>從左側選單挑選要看的縣市長</h5>
-          <h4 style={styles.welcomeSection.h4}>貼上新聞連結，更新施政進度</h4>
-          <h5 style={styles.welcomeSection.h5}>進度開放編輯，只要附上訊息出處，就能更新施政進度</h5>
-          <ListDivider style={styles.welcomeSection.divider}/>
-          <ListItem primaryText={<h3 style={styles.welcomeSection.h3}>我知道了</h3>}
-            leftIcon={<DoneIcon style={styles.welcomeSection.doneIcon}/>} />
-        </Card>
+        {this.state.wasVisited ? null : <WelcomeSection ref="welcomeSection"
+          onKnownTouchTap={::this.onKnownTouchTap}/>}
         <Card style={styles.recentUpdate.root}>
           <CardTitle title="近期更新"
             titleStyle={styles.recentUpdate.title} />
@@ -176,6 +150,6 @@ var Home = React.createClass({
       </div>
     );
   }
-});
+}
 
 module.exports = Transmit.createContainer(Home, {});
