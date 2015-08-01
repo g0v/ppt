6115cac3e8941f5e3fbd2a1f09f40a504ded26aa
transedward <transedward@gmail.com>
2015-08-01 20:43:32 +0800

    fix on header click, and cleanup

--- common/views/Sidebar.jsx
+++ common/views/Sidebar.jsx
@@ -21,7 +21,6 @@ class Sidebar extends React.Component {
   constructor(props, context) {
     super(props, context);
     this.toggle = this.toggle.bind(this);
-    this._onLeftNavChange = this._onLeftNavChange.bind(this);
   }
 
   getStyles() {
@@ -41,7 +40,6 @@ class Sidebar extends React.Component {
   }
 
   toggle() {
-    debugSideBar('toggle called');
     this.refs.leftNav.toggle();
   }
 
@@ -50,14 +48,13 @@ class Sidebar extends React.Component {
   }
 
   _onHeaderClick() {
-    this.context.executeAction(navigateAction, '/');
+    this.context.executeAction(navigateAction, {url: '/'});
     this.refs.leftNav.close();
   }
 
   render(){
-
     var header = (
-      <div style={this.getStyles()} onClick={this._onHeaderClick}>
+      <div style={this.getStyles()} onTouchTap={::this._onHeaderClick}>
         政治承諾追蹤網
       </div>
     );
@@ -69,7 +66,7 @@ class Sidebar extends React.Component {
         docked={false}
         isInitiallyOpen={false}
         menuItems={menuItems}
-        onChange={this._onLeftNavChange}
+        onChange={::this._onLeftNavChange}
         style={{zDepth: 1}}/>
     );
   }
@@ -80,95 +77,3 @@ Sidebar.contextTypes = {
 };
 
 module.exports = Sidebar;
-
-/* initial state
-governors: [
-  {
-    name: "台中市政府",
-    avatar: "http://semantic-ui.com/images/avatar/small/daniel.jpg",
-    title: '台中市長',
-    promiseCount: 53
-  },
-  {
-    name: "台北市政府",
-    avatar: "http://semantic-ui.com/images/avatar/small/daniel.jpg",
-    title: '台北市長',
-    promiseCount: 62
-  }
-]
-*/
-
-/* governors.map
-return (
-  <NavLink href={"/governor/" + governor.name} className="item" style={listItemStyle} key={idx}>
-    <img className="ui avatar image" src="http://semantic-ui.com/images/avatar/small/daniel.jpg"/>
-    <div className="content">
-      <div className="header" style={headerStyle}>{governor.name}</div>
-      <div className="description" style={descriptionStyle}>{governor.title}・2014-2018・{governor.promiseCount} 承諾</div>
-    </div>
-  </NavLink>
-);
-*/
-
-/* style part
-var sidebarTextColor = 'rgba(255, 255, 255, .82)';
-
-var listStyle = {
-  // background: 'transparent',
-},
-listItemStyle = {
-  padding: 8
-},
-segmentStyle = {
-  // padding: 8,
-},
-segmentContainerStyle = {
-  boxShadow: 'none',
-  background: 'transparent',
-  color: sidebarTextColor
-},
-headerStyle = {
-  color: sidebarTextColor
-},
-descriptionStyle = {
-  fontSize: 12,
-  color: sidebarTextColor
-};
-*/
-
-/* return part
-<div className="ui large green inverted vertical sidebar menu">
-  <div className="ui segment" style={segmentContainerStyle}>
-    <section className="ui vertical segment" style={segmentStyle}>
-      <div className="ui list" style={listStyle}>
-        {governorElems}
-      </div>
-    </section>
-
-    <section className="ui vertical segment" style={segmentStyle}>
-      <div className="ui list" style={listStyle}>
-        <a className="item" style={listItemStyle}>
-          <img className="ui avatar image" src="http://semantic-ui.com/images/avatar/small/daniel.jpg"/>
-          <div className="content">
-            <div className="header" style={headerStyle}>新增施政單位⋯⋯</div>
-          </div>
-        </a>
-
-        <NavLink href="/about" className="item" style={listItemStyle}>
-          <img className="ui top avatar image" src="http://semantic-ui.com/images/avatar/small/daniel.jpg"/>
-          <div className="content">
-            <div className="header" style={headerStyle}>關於政治承諾追蹤網</div>
-          </div>
-        </NavLink>
-
-        <NavLink href="/" className="item" style={listItemStyle}>
-          <img className="ui top avatar image" src="http://semantic-ui.com/images/avatar/small/daniel.jpg"/>
-          <div className="content">
-            <div className="header" style={headerStyle}>回到首頁</div>
-          </div>
-        </NavLink>
-      </div>
-    </section>
-  </div>
-</div>
-*/
