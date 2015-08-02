c94949ca5a5493dceffd6a9c96dce9c5497962bd
transedward <transedward@gmail.com>
2015-08-02 17:25:24 +0800

    makes loading page looks better

--- common/views/Loading.jsx
+++ common/views/Loading.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { CircularProgress } from 'material-ui';
+import pptSpacing from '../styles/spacing';
+import pptColors from '../styles/color';
 
 export default class Loading extends React.Component {
+
+  getStyles() {
+    return {
+      root: {
+        paddingTop: pptSpacing.appBarHeight + 26,
+        height: '100%',
+        width: '100%'
+      }
+    };
+  }
+
   render () {
-    return (<div>Loading...</div>);
+    const styles = this.getStyles();
+    return (
+      <div style={styles.root}>
+        <div style={{width: '50%', margin: 'auto', textAlign: 'center'}}>
+          <div style={{fontSize: 20, color: pptColors.lightBlack}}>Loading...</div>
+          <CircularProgress  size={2} mode="indeterminate" color={pptColors.primaryBlue}/>
+        </div>
+      </div>
+    );
   }
 }
